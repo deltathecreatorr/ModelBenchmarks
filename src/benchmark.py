9a4d3b2499608d6b57bbea909e54739f177e6e48
deltathecreatorr <julio_15@hotmail.co.uk>
2026-01-29 16:32:47 +0000

    Update .gitignore and requirements; modify benchmark.py for YOLO model integration

--- src/benchmark.py
+++ src/benchmark.py
@@ -2,26 +2,39 @@ from transformers import AutoImageProcessor, AutoModelForObjectDetection
 from PIL import Image
 import torch
 
+from ultralytics import YOLO
+
 models_to_benchmark =[
     "hustvl/yolos-tiny",
-    "PekingU/rtdetr_r101vd_coco_o365"
+    "PekingU/rtdetr_r18vd_coco_o365"
 ]
 
-processor = AutoImageProcessor.from_pretrained(models_to_benchmark[1])
-model = AutoModelForObjectDetection.from_pretrained(models_to_benchmark[1])
+ultralytics_model = [
+    "yolo26n.pt",
+    "yolo11n.pt",
+    "yolov10n.pt",
+    "yolov8n.pt",
+]
 
-url = "object\\couch.png"
-image = Image.open(url).convert("RGB")
+# processor = AutoImageProcessor.from_pretrained(models_to_benchmark[1])
+# model = AutoModelForObjectDetection.from_pretrained(models_to_benchmark[1])
 
-inputs = processor(images=image, return_tensors="pt")
+# url = "object\\couch.png"
+# image = Image.open(url).convert("RGB")
 
-with torch.no_grad():
-    outputs = model(**inputs)
+# inputs = processor(images=image, return_tensors="pt")
 
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+# with torch.no_grad():
+#     outputs = model(**inputs)
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    class_name = model.config.id2label[label.item()]
-    print(f"Detected {class_name} with {round(score.item(), 3)} confidence at location {box}")
+# target_sizes = torch.tensor([image.size[::-1]])
+# results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+# for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+#     box = [round(i, 2) for i in box.tolist()]
+#     class_name = model.config.id2label[label.item()]
+#     print(f"Detected {class_name} with {round(score.item(), 3)} confidence at location {box}")
+
+model = YOLO(ultralytics_model[0])
+source = "object\\couch.png"
+model.predict(source, save=True)
