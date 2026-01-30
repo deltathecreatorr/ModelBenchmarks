bb0703ba0905d1990c0490c6c83598f0e38e0582
deltathecreatorr <julio_anandaraaj@outlook.com>
2026-01-30 16:02:56 +0000

    Conversion of hugging face models to onnx models to be run on raspbian VM

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,4 +1,6 @@
-from transformers import AutoImageProcessor, AutoModelForObjectDetection
+from transformers import AutoImageProcessor, AutoModelForObjectDetection, AutoTokenizer
+from pathlib import Path
+
 from PIL import Image
 import torch
 
@@ -13,27 +15,29 @@ ultralytics_model = [
     "yolo26n.pt",
     "yolo11n.pt",
     "yolov10n.pt",
-    "yolov8n.pt",
+    "yolov8n.pt"
 ]
 
-processor = AutoImageProcessor.from_pretrained(models_to_benchmark[1])
-model = AutoModelForObjectDetection.from_pretrained(models_to_benchmark[1])
 
-url = "object\\couch.png"
-image = Image.open(url).convert("RGB")
 
-inputs = processor(images=image, return_tensors="pt")
+# processor = AutoImageProcessor.from_pretrained(models_to_benchmark[1])
+# model = AutoModelForObjectDetection.from_pretrained(models_to_benchmark[1])
 
-with torch.no_grad():
-    outputs = model(**inputs)
+# url = "object\\couch.png"
+# image = Image.open(url).convert("RGB")
 
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+# inputs = processor(images=image, return_tensors="pt")
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    class_name = model.config.id2label[label.item()]
-    print(f"Detected {class_name} with {round(score.item(), 3)} confidence at location {box}")
+# with torch.no_grad():
+#     outputs = model(**inputs)
+
+# target_sizes = torch.tensor([image.size[::-1]])
+# results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+# for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+#     box = [round(i, 2) for i in box.tolist()]
+#     class_name = model.config.id2label[label.item()]
+#     print(f"Detected {class_name} with {round(score.item(), 3)} confidence at location {box}")
 
 # model = YOLO(ultralytics_model[3])
 # success = model.export(format="onnx")  # export the model to ONNX format
