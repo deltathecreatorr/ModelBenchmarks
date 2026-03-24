641435a4c959e76d8826d41987e00e83f50ec6ce
deltathecreatorr <julio_anandaraaj@outlook.com>
2026-03-24 20:44:10 +0000

    Update README and benchmark script; enhance documentation and add model details

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-This repository holds the benchmarking of pretrained CNN models for identifying objects. The model is to identify common objects within a standard household environment. The model that is the most suitable for a Raspberry Pi 5 8GB embedded system will be transferred using ONNX Runtime. The problem of real time object detection has many applications of neural networks being applied to it, and it has been recently found out that RT-DETRs often have a greater accuracy than YOLO models. While this is true for seemingly unconstrained systems, using a deep learning model on a mobile system has created the problem of making sure the model can run within strict constraints.
+This repository holds the benchmarking of pretrained CNN models for identifying objects. The model is to identify common objects within a standard household environment. The model that is the most suitable for a Raspberry Pi 5 8GB embedded system will be transferred using ONNX Runtime. The problem of real time object detection has many applications of neural networks being applied to it, and it has been recently found out that RT-DETRs often have a greater accuracy than YOLO models. While this is true for seemingly unconstrained systems, using a deep learning model on a mobile system has created the problem of making sure the model can run within strict constraints. RT-DETRs are not stable enough to be run on edge computing devices, and they do not handle quantisation or compression do their complex structure.
 
 ## Pre-requisites
 
@@ -12,6 +12,27 @@ After cloning this repository, install all dependencies onto a virtual environme
 ## Running
 
 Simply run the code after cloning and installing dependencies, this program will use a CUDA supported GPU if available.
+
+## Models Used
+
+The models used are as follows:
+
+- YOLOv10n
+- YOLOv8n
+- YOLOv26n
+- YOLOv11n
+- YOLOv5n
+- YOLOv6nlite
+- NanoDet-Plus_M
+- PicoDet_S
+
+All of the models were converted to ONNX format for running on edge computing device. The YOLO models were also suitable for quantisation and have been quantised to uint8 input type.
+
+## Results
+
+![Plots of Results on Nano models running on a Pi 5 8GB](improved_hardware_benchmark.png)
+
+![Plot of Power usage from Models running on Pi 5 8GB from UPS HAT B from Waveshare](power_battery_benchmark.png)
 
 ## Author
 
--- src/benchmark.py
+++ src/benchmark.py
@@ -12,30 +12,28 @@ import time
 import sys
 import seaborn as sns
 from ina219 import INA219
+
+# 
 SHUNT_OHMS = 0.1
 
+# Checking if the INA219 sensor is connected to the I2C bus.
 try:
     # Initialize the sensor
     ina = INA219(SHUNT_OHMS, busnum=1, address=0x42)
     ina.configure() # This automatically handles all the complex calibration math!
-    print("✅ Power Sensor configured successfully!")
+    print("Power Sensor configured successfully!")
 except Exception as e:
-    print(f"⚠️ Failed to initialize Power Sensor: {e}")
+    print(f"Failed to initialize Power Sensor: {e}")
     ina = None
 
-COCO_MAPPING = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
-    22, 23, 24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 
-    43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 
-    62, 63, 64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 
-    85, 86, 87, 88, 89, 90
-]
-
+#COCO validation dataset paths
 images_folder = './data/val2017'
 annotations_file = './data/annotations/instances_val2017.json'
 
+# Loading the COCO val_set using torchvision
 coco_data = torchvision.datasets.CocoDetection(root=images_folder, annFile=annotations_file)
 
+#Model paths for each ONNX model for the benchmark. Some of the models have been quanitsed as this speeds up inference.
 model_paths = {
     "yolov10n": './onnx/quantized/yolov10n_int8.onnx',
     "yolov8n": './onnx/quantized/yolov8n_int8.onnx',
@@ -47,26 +45,48 @@ model_paths = {
     "picodet_s": './onnx/picodet_s_320.onnx'
 }
 
+#ONNX Runtime session options to optimise for ARM CPU inference.
+#Also limits number of cores to 2 as Pi has 4 cores but also need to run SLAM algorithm.
 session_options = onnxruntime.SessionOptions()
 session_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
 session_options.intra_op_num_threads = 2
 session_options.inter_op_num_threads = 1
 session_options.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
 
+#Get images from the COCO dataset with the number of images and a random seed to ensure reproducibility.
 def gather_images(num_images=10, seed=42):
+    """
+    Gathers a specified number of random images from the COCO dataset for benchmarking.
+    Arguments:
+        num_images: Number of random images to gather.
+        seed: Random seed for reproducibility.
+    Returns:
+        A list of tuples containing the image and its corresponding ground truth annotations.
+    """
     random.seed(seed)
     images = []
     for i in range(num_images):
         random_index = random.randint(0, len(coco_data) - 1)
-        img, ground_truth = coco_data[random_index]
+        img, ground_truth = coco_data[random_index] #Gather image and the label associated with the image
         images.append((img, ground_truth))
-    
-    
     return images
 
+#Gathering 100 random images from the COCO val2017 dataset for benchmarking. This will be used to run inference on each model and measure latency, CPU usage, RAM usage, temperature, and power consumption.
 batch_images_1 = gather_images(num_images=100, seed=42)
 
+#preprocessing the image for inference.
+#Resizes the image to the input size of the model dynamically.
+#Converts the image to the correct format and data type.
 def preprocess_image(image, model_session):
+    """
+    Preprocesses the input image for the ONNX model.
+    Dynamically resizes the image to match the model's expected input size and format.
+    Arguments:
+        image: PIL Image to preprocess.
+        model_session: ONNX Runtime session to extract input details.
+    Returns:
+        Preprocessed image as a numpy array ready for inference.
+    """
     model_input = None
     for inp in model_session.get_inputs():
         if len(inp.shape) == 4 or inp.name in ['image', 'images', 'input_tensor','x', 'serving_default_images:0']:
@@ -95,13 +115,13 @@ def preprocess_image(image, model_session):
 
     if target_type == "tensor(float)":
         processed_img = resized_img.astype(np.float32) / 255.0
-    elif target_type == "tensor(uint8)":
+    elif target_type == "tensor(uint8)": #some of the models have been quantized to uint8 as this speeds up inference time, but can cause issues for some architectures.
         processed_img = resized_img.astype(np.uint8)
     else:
         raise ValueError(f"Unsupported input type: {target_type}")
 
     if is_chw:
-        processed_img = processed_img.transpose(2, 0, 1)
+        processed_img = processed_img.transpose(2, 0, 1) #change the image shape depending on the model.
 
     return np.expand_dims(processed_img, axis=0).astype(np.float32)
 
@@ -109,11 +129,16 @@ def wait_for_cooldown(target_temp=63.0, timeout=300):
     """
     Pauses execution until the Pi cools down to the target temperature.
     timeout: Maximum seconds to wait before giving up and continuing.
+    Arguments:
+        target_temp: Temperature in Celsius to cool down to before starting the next model.
+        timeout: Maximum time in seconds to wait for cooldown before forcing start.
+    Returns:
+        None
     """
     current_temp = get_pi_temp()
     
     if current_temp <= target_temp:
-        return # Already cool enough!
+        return
         
     print(f"Heat Soak Detected ({current_temp:.1f}°C). Cooling down to {target_temp}°C...")
     
@@ -132,6 +157,13 @@ def wait_for_cooldown(target_temp=63.0, timeout=300):
         time.sleep(5) # Check the temperature every 5 seconds
 
 def get_pi_temp():
+    """
+    Reads the CPU temp of the Pi from the system file.
+    Arguments:
+        None
+    Returns:
+        Current CPU temperature in Celsius as a float.
+    """
     try:
         with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
             temp_c = float(f.read().strip()) / 1000.0
@@ -141,14 +173,20 @@ def get_pi_temp():
         
     try:
         import subprocess
-        temp_str = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8')
+        temp_str = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8') # Read the temperature using vcgencmd as a fallback method
         return float(temp_str.replace('temp=', '').replace('\'C\n', ''))
     except Exception:
         print("Could not read temperature sensor. Defaulting to 40C.")
         return 40.0
 
 def get_safe_power_metrics():
-    """Reads live Watts and estimates Battery % directly from the chip."""
+    """
+    Reads live Watts directly from the chip from UPS Hat B.
+    Arguments:
+        None
+    Returns:
+        Power in Watts as a float.
+    """
     if ina is None:
         return 0.0, 0.0
         
@@ -157,18 +195,20 @@ def get_safe_power_metrics():
         voltage_V = ina.voltage()
         power_mW = ina.power()
         power_W = power_mW / 1000.0  # Convert to Watts
-        
-        # Calculate battery percentage (4.2V is full, 3.2V is dead)
-        battery_percent = ((voltage_V - 3.2) / (4.2 - 3.2)) * 100.0
-        battery_percent = max(0.0, min(100.0, battery_percent)) 
-        
-        return float(power_W), float(battery_percent)
-        
+        return float(power_W)
     except Exception as e:
         print(f"I2C Read Error: {e}")
         return 0.0, 0.0
 
 def benchmark_model(images):
+    """
+    Runs the main inference loop for the ONNX models and captures latency, CPU usage, RAM usage, temp and power usage.
+    Arguments:
+        images: List of tuples containing the image and its corresponding ground truth annotations.
+    Returns:
+        A dictionary containing the benchmark results for each model, including per-image latency, CPU usage, RAM usage, temperature, and power usage.
+    """
+    # Get the current process to measure RAM usage.
     process = psutil.Process(os.getpid())
     benchmark_results = {}
 
@@ -194,23 +234,27 @@ def benchmark_model(images):
             else:
                 warmup_dict[inp.name] = warmup_data
 
+        # Warmup runs to stabilise performance and make sure any model initialisation is out of the way.
         for _ in range(10):
             try: session.run(None, warmup_dict)
             except: pass
 
+        #Initialise lists to store per-image data values.
         per_image_times = []
         per_image_cpu = []
         per_image_ram = []
         per_image_temp = []
         per_image_power = []
-        per_image_battery = []
-
+        
+        # Get the baseline CPU usage before starting inference.
         psutil.cpu_percent(interval=None)
 
         for img, ground_truth in images:
+            # Preprocess image before inference
             input_data = preprocess_image(img, session)
             
             input_dict = {}
+            # Build the input dictionary based on the model inputs.
             for inp in session.get_inputs():
                 if inp.name in ['image', 'images', 'input_tensor', 'x', 'serving_default_images:0']:
                     input_dict[inp.name] = input_data
@@ -227,27 +271,30 @@ def benchmark_model(images):
                     input_dict[inp.name] = input_data
 
             try:
-                start_time = time.perf_counter()
+                #Main data gathering loop
+                start_time = time.perf_counter() # Start the timer right before inference
                 outputs = session.run(None, input_dict)
-                end_time = time.perf_counter()
+                end_time = time.perf_counter() # End the time right after inference to get the most accurate latency measurement.
 
-                current_temp = get_pi_temp()
-
+                current_temp = get_pi_temp() # Gather the CPU temp right after inference to see the impact of the model on the temperature.
+                
+                # Get the CPU and RAM usage 
                 current_cpu = psutil.cpu_percent(interval=None)
                 current_ram = process.memory_info().rss / (1024 * 1024)
 
-                current_power, battery_percent = get_safe_power_metrics()
+                #Measure the current power usage in Watts
+                current_power= get_safe_power_metrics()
 
                 per_image_times.append(end_time - start_time)
                 per_image_cpu.append(current_cpu)
                 per_image_ram.append(current_ram)
                 per_image_temp.append(current_temp)
                 per_image_power.append(current_power)
-                per_image_battery.append(battery_percent)
 
             except Exception as e:
                 print(f"Error running {model_name} on image: {e}")
 
+        # Get the resolution of the input image for this model during warmup as it can affect results.
         shape = warmup_data.shape
         if len(shape) >= 4 and shape[1] == 3:
             h, w = shape[2], shape[3]
@@ -260,7 +307,6 @@ def benchmark_model(images):
             "per_image_ram": per_image_ram,
             "per_image_temp": per_image_temp,
             "per_image_power": per_image_power,
-            "per_image_battery": per_image_battery,
             "resolution": (h, w)
 
         }
@@ -268,7 +314,14 @@ def benchmark_model(images):
     return benchmark_results
 
 def plot_graphs(results):
-
+    """
+    Plots the graphs for latency, CPU usage, RAM usage, and temp change over time from benchamrk script results.
+    Arguments:
+        results: A dictionary containing the benchmark results for each model, including per-image latency, CPU
+    Returns:
+        None but saves the plot as an image file.
+    """
+    #Check if any models ran successfully and have data.
     successful_models = [m for m in results.keys() if len(results[m]['per_image_times']) > 0]
     
     if not successful_models:
@@ -278,6 +331,7 @@ def plot_graphs(results):
     print(f"Plotting data for: {successful_models}")
 
     latencies = []
+    #Plot latency graphs from data with all successful models.
     for m in successful_models:
         h, w = results[m]['resolution']
         megapixels = (h * w) / 1_000_000.0
@@ -285,17 +339,22 @@ def plot_graphs(results):
         norm_times = [(t * 1000) / megapixels for t in results[m]['per_image_times']]
         latencies.append(norm_times)
 
+    #Plot CPU usage data from all successful models.
     cpus = [results[m]['per_image_cpu'] for m in successful_models]
     
+    # Get average RAM usage for each model to plot.
     avg_rams = [sum(results[m]['per_image_ram']) / len(results[m]['per_image_ram']) for m in successful_models]
 
+    # Get change in temperature to plot against time.
     temps = [results[m]['per_image_temp'] for m in successful_models]
-
+    
+    # 4 row subplot of CPU, RAM, Temp and Latency.
     fig, axes = plt.subplots(4, 1, figsize=(8, 20), constrained_layout=True)
     fig.suptitle('CPU, RAM, Temp, Latency Measurements', fontsize=16, fontweight='bold')
 
     colors = plt.cm.tab10(np.linspace(0, 1, len(successful_models)))
 
+    # Beeswarm plot for latency to show the distribution of latency values per image.
     axes[0].set_title('Normalized Latency (Every Single Frame)')
     axes[0].set_ylabel('Latency per Megapixel (ms/MP)')
     sns.stripplot(data=latencies, ax=axes[0], palette='tab10', size=3, jitter=True, alpha=0.6)
@@ -303,12 +362,14 @@ def plot_graphs(results):
     axes[0].set_xticklabels(successful_models, rotation=45)
     axes[0].grid(True, axis='y', linestyle='--', alpha=0.5)
 
+    # Box plot for CPU usage to show the average CPU load for each model including spikes in CPU usage as part of the box plot.
     bplot2 = axes[1].boxplot(cpus, tick_labels=successful_models, patch_artist=True, showfliers=False)
     axes[1].set_title('CPU Load (Baseline & Spikes)')
     axes[1].set_ylabel('CPU Usage (%)')
     axes[1].tick_params(axis='x', rotation=45)
     axes[1].grid(True, linestyle='--', alpha=0.5)
 
+    # bar plot for RAM usage
     axes[2].bar(successful_models, avg_rams, color=colors, edgecolor='black')
     axes[2].set_title('Average RAM Footprint')
     axes[2].set_ylabel('Megabytes (MB)')
@@ -316,6 +377,7 @@ def plot_graphs(results):
     axes[2].grid(True, axis='y', linestyle='--', alpha=0.5)
     axes[2].set_ylim(bottom=400)
 
+    #Temp change line plot to show the overall heating trend of the CPU, the initial change will have different starting temperatures.
     axes[3].set_title('Overall CPU Heating Trend')
     axes[3].set_ylabel('Temp Increase (°C)')
     axes[3].set_xlabel('Images Processed (Time)')
@@ -360,12 +422,15 @@ def plot_graphs(results):
     plt.savefig(image_name, dpi=300, bbox_inches='tight')
     print(f"Dashboard saved as '{image_name}'")
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 def plot_power_and_battery(results):
-    # Check if we successfully captured battery data
-    successful_models = [m for m in results.keys() if len(results[m].get('per_image_battery', [])) > 0]
+    """
+    Plots the average power consumption for each model from the benchmark script.
+    Arguments:
+        results: A dictionary containing the benchmark results for each model, including per-image power usage.
+    Returns:
+        None but saves the plot as an image file.
+    """
+    successful_models = [m for m in results.keys()]
     
     if not successful_models:
         print("No power data found! Did the Waveshare sensor disconnect?")
@@ -378,6 +443,7 @@ def plot_power_and_battery(results):
 
     avg_powers = []
     
+    #Plot average power usage for each model as bar plot to compare the average usage.
     for idx, model in enumerate(successful_models):
         power_watts = results[model]['per_image_power']
         latency_secs = results[model]['per_image_times']
@@ -397,6 +463,7 @@ def plot_power_and_battery(results):
     plt.savefig(image_name, dpi=300, bbox_inches='tight')
     print(f"Power Dashboard saved as '{image_name}'")
 
+# Run the benchmark and plot.
 results_data = benchmark_model(batch_images_1)
 plot_graphs(results_data)
 plot_power_and_battery(results_data)
